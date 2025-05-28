a95ce2dcc54c8a31ad4c631642bcf5338c76fe0a
Kunjai10 <35565552@student.murdoch.edu.au>
2025-05-28 19:09:53 +0800

    Delete setup.sh

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Script to install Apache and enable web server
-sudo apt update
-sudo apt install apache2 -y
-sudo ufw allow 'Apache Full'
-sudo systemctl enable apache2
-sudo systemctl start apache2
